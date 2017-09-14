6c695a89fee89c7e902e2d260e8b294ca104526e
petez <pete.zsolt@codeyard.eu>
2017-09-14 23:30:05 +0200

    ➕ Added new classes

---
+++ BeerPong/MatchDetails/MatchDetailsMasterView.swift
@@ -0,0 +1,21 @@
+//
+//  MatchDetailsMasterView.swift
+//  BeerPong
+//
+//  Created by Zsolt Pete on 2017. 09. 14..
+//  Copyright © 2017. Zsolt Pete. All rights reserved.
+//
+
+import UIKit
+
+class MatchDetailsMasterView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
