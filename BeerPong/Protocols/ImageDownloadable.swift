182dc2ee07eb01edeeac29f45ee4e2eb2abec677
petez <pete.zsolt@codeyard.eu>
2017-09-16 22:14:02 +0200

    ✨ Protocol implemented

--- BeerPong/Protocols/ImageDownloadable.swift
+++ BeerPong/Protocols/ImageDownloadable.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+import UIKit
+import SDWebImage
+import RxCocoa
+import RxSwift
+
+protocol ImageDownloadable {
+    
+    func download(from url: String, to image: Variable<UIImage>)
+    
+}
+
+extension ImageDownloadable{
+    
+    func download(from url: String, to image: Variable<UIImage>) {
+        SDWebImageManager.shared().downloadImage(with: URL(string: url), options: .progressiveDownload, progress:nil) { (maybeImage: UIImage?, maybeError, cacheType, finished: Bool, imageURL) in
+            if maybeImage != nil  && finished == true{
+                image.value = maybeImage!
+            }
+        }
+    }
+    
+}
