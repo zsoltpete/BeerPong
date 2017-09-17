bb65f16419259150c9718d3564556ca7e4f32e70
petez <pete.zsolt@codeyard.eu>
2017-09-17 22:15:47 +0200

    ➕ Added new files

---
+++ BeerPong/Profile/ProfileMasterView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileMasterView.swift
+//  BeerPong
+//
+//  Created by Zsolt Pete on 2017. 09. 17..
+//  Copyright © 2017. Zsolt Pete. All rights reserved.
+//
+
+import UIKit
+
+class ProfileMasterView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
