3eebc6aecefcf9314d9834b7fab2408946b27181
petez <pete.zsolt@codeyard.eu>
2017-09-19 22:19:33 +0200

    ➕ Added new classes

---
+++ BeerPong/UserSearchResult/UserSearchResultCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserSearchResultCell.swift
+//  BeerPong
+//
+//  Created by Zsolt Pete on 2017. 09. 19..
+//  Copyright © 2017. Zsolt Pete. All rights reserved.
+//
+
+import UIKit
+
+class UserSearchResultCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ BeerPong/UserSearchResult/UserSearchResultViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  UserSearchResultViewModel.swift
+//  BeerPong
+//
+//  Created by Zsolt Pete on 2017. 09. 19..
+//  Copyright © 2017. Zsolt Pete. All rights reserved.
+//
+
+import UIKit
+
+class UserSearchResultViewModel: NSObject {
+
+}
